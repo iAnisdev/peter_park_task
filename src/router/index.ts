c9ea60bf651d1c06cbef530ef3e2911e918763f1
marahmanjs <imarahmandev@gmail.com>
2022-07-21 00:27:50 +0500

    adding tailwind css

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,6 +8,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/create',
+      name: 'create',
+      component: () => import('../views/createView.vue')
     }
   ]
 })
